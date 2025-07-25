d7153d5e0390482ff0754582ba3d39e603429633
Supriyo-Giri60 <supriyogiri803@gmail.com>
2025-07-26 01:53:55 +0530

    some new changes are made

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const app = express();
-require('dotenv').config();
-
+const cors = require("cors");
+const bodyParser = require('body-parser');
 const PORT =  process.env.PORT || 8080;
 
+const app = express();
+const userRoutes = require("./Routes/authRoutes");
+require('dotenv').config();
+require('./Models/db'); 
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/auth',userRoutes);
+
 app.listen(PORT, ()=>{
-    console.log(`Server is running at PORT: ${PORT}`);
+    console.log(`Server is running at PORT: ${PORT} ...`);
 })
